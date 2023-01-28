dc22bc2fa4d35479d1a6160f87d0b9a13a5b5814
ExcaliburSims <ximeonzilu@gmail.com>
2023-01-28 08:06:32 +0100

    feat: connect database

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,22 @@
 import express from "express";
 import { graphqlHTTP } from "express-graphql";
 import cors from "cors";
-import { createConnection } from "typeorm";
+import { DataSource } from "typeorm";
 
 const main = async () => {
+  const AppDataSource = new DataSource({
+    type: "mysql",
+    host: "localhost",
+    port: 3306,
+    username: "root",
+    password: "password",
+    database: "graphCRUD",
+    entities: [],
+    synchronize: false,
+    logging: true,
+  });
+  await AppDataSource.initialize();
+
   const app = express();
   app.use(cors());
   app.use(express.json());
